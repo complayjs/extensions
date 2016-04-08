25f702f81795af7a69c66bfd1df4f8bf0bfb888b
HenriPodolski <henrip@gmail.com>
2016-04-08 12:27:15 +0200

    decouples components and services from modules

--- fallback/fallback.js
+++ fallback/fallback.js
@@ -1,6 +1,11 @@
 export default function(type) {
 	return function() {
-		console.warn(`Plugin engine for type "${type}" not implemented yet.`);
+		let msgArray = [
+			`Extension for "${type}" is not configured yet.\r\n`, 
+			`Please pass an extensions through ApplicationFacade constructor options.${type}\r\n`, 
+			`or directly through Module, Service or Component via options.app.${type}!`
+		];
+		console.warn(msgArray.join(''));
 		return arguments[0];
 	}
 }
