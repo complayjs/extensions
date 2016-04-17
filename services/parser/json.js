971f3ae2079f6d3bb414d1be2c09208100ad62d0
HenriPodolski <henrip@gmail.com>
2016-04-17 20:29:14 +0200

    fixes js error

--- services/parser/json.js
+++ services/parser/json.js
@@ -1,8 +1,8 @@
-export default {
+export default class JsonParserExtension {
 	
 	parse(rawData) {
 		return JSON.parse(rawData);
-	},
+	}
 
 	stringify(jsonData) {
 		return JSON.stringify(jsonData);
