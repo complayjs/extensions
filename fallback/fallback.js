4358ef52f6284c590c56c67d1f6fb5df379d148a
Henri Podolski <henrip@gmail.com>
2016-05-20 17:08:50 +0200

    Prevent displaying "not configured warnings" for every instance

--- fallback/fallback.js
+++ fallback/fallback.js
@@ -1,11 +1,22 @@
+let typeMessagesBroadcasted = [];
+
 export default function(type) {
 	return function() {
-		let msgArray = [
-			`Extension for "${type}" is not configured yet.\r\n`, 
-			`Please pass an extensions through ApplicationFacade constructor options.${type}\r\n`, 
-			`or directly through Module, Service or Component via options.app.${type}!`
-		];
-		console.warn(msgArray.join(''));
+
+		let isString = typeof type === 'string';
+
+		if (isString && typeMessagesBroadcasted.indexOf(type) === -1) {
+			let msgArray = [
+				`Extension for "${type}" is not configured yet.\r\n`,
+				`Please pass an extensions through ApplicationFacade constructor options.${type}\r\n`,
+				`or directly through Module, Service or Component via options.app.${type}!`
+			];
+
+			console.warn(msgArray.join(''));
+
+			typeMessagesBroadcasted.push(type);
+		}
+
 		return arguments[0];
 	}
 }
