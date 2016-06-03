7a387c0cf134019b5509cd16c2395854c911bca1
Henri Podolski <henrip@gmail.com>
2016-06-03 19:40:12 +0200

    Fix vent for using with multiple instances

--- vent/vent.js
+++ vent/vent.js
@@ -1,31 +1,36 @@
-let target;
+let targets = [];
 let events = {};
 
 export default function Vent(newTarget){
-	var empty = [];
+	let empty = [];
+	let index = targets.indexOf(newTarget);
+	let target = targets[index];
 
-	if (typeof target === 'undefined' || newTarget !== target) {
+	if (index === -1 || !target) {
 		target = newTarget || this;
 
-		if (!target.name) {
-			target.name = Math.random() + '';
+		if (!target.uid) {
+			target.uid = Math.random() + '';
 		}
 
-		events[target.name] = {};
+		targets.push(target);
+		index = targets.length - 1;
+
+		events[targets[index].uid] = {};
 	}
 
 	/**
 	 *  On: listen to events
 	 */
 	target.on = function(type, func, ctx){
-		(events[target.name][type] = events[target.name][type] || []).push([func, ctx]);
+		(events[targets[index].uid][type] = events[targets[index].uid][type] || []).push([func, ctx]);
 	}
 	/**
 	 *  Off: stop listening to event / specific callback
 	 */
 	target.off = function(type, func){
-		type || (events[target.name] = {})
-		var list = events[target.name][type] || empty,
+		type || (events[targets[index].uid] = {})
+		let list = events[targets[index].uid][type] || empty,
 				i = list.length = func ? list.length : 0;
 		while(i--) func == list[i][0] && list.splice(i,1)
 	}
@@ -33,9 +38,9 @@ export default function Vent(newTarget){
 	 * Trigger: send event, callbacks will be triggered
 	 */
 	target.trigger = function(type){
-		var list = events[target.name][type] || empty, i=0, j;
+		let list = events[targets[index].uid][type] || empty, i=0, j;
 		while(j=list[i++]) j[0].apply(j[1], empty.slice.call(arguments, 1))
 	};
 
-	return target;
+	return targets[index];
 }
